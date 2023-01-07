01d6f5154c9804656a1997859cf9869bc2cba3ce
Singh <daljeet.test99@gmail.com>
2023-01-07 15:49:28 +0100

    Add Eureka Client to Inventory module and enble multiple instance inside intellij

--- inventory-service/src/main/java/com/singh/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/singh/inventoryservice/InventoryServiceApplication.java
@@ -2,7 +2,6 @@ package com.singh.inventoryservice;
 
 import com.singh.inventoryservice.model.Inventory;
 import com.singh.inventoryservice.repository.InventoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- inventory-service/src/main/java/com/singh/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/singh/inventoryservice/controller/InventoryController.java
@@ -1,8 +1,12 @@
 package com.singh.inventoryservice.controller;
 
+import com.netflix.discovery.shared.Application;
 import com.singh.inventoryservice.dto.InventoryResponse;
 import com.singh.inventoryservice.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,4 +24,5 @@ public class InventoryController {
     public List<InventoryResponse> IsInStock(@RequestParam List<String> skuCode){
         return inventoryService.IsInStock(skuCode);
     }
+
 }
